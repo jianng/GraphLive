f8bbbf5669e15b07416df547b33a0f72bff80dd4
jianng <jiangbo99@outlook.com>
2013-06-26 00:43:20 +0800

    完成GraphLive主体结构

--- src/GraphLive.js
+++ src/GraphLive.js
@@ -1,34 +1,34 @@
 var CP = window.CanvasRenderingContext2D && CanvasRenderingContext2D.prototype;
-if (CP && CP.lineTo) CP.dashedLine = function(x, y, x2, y2, dashArray) {
-	if (!dashArray) dashArray = [10, 5];
-	var dashCount = dashArray.length;
-	var dx = (x2 - x);
-	var dy = (y2 - y);
-	var xSlope = (Math.abs(dx) > Math.abs(dy));
-	var slope = (xSlope) ? dy / dx : dx / dy;
+if (CP && CP.lineTo) CP.dashedLine = function (x, y, x2, y2, dashArray) {
+		if (!dashArray) dashArray = [10, 5];
+		var dashCount = dashArray.length;
+		var dx = (x2 - x);
+		var dy = (y2 - y);
+		var xSlope = (Math.abs(dx) > Math.abs(dy));
+		var slope = (xSlope) ? dy / dx : dx / dy;
 
-	this.moveTo(x, y);
-	var distRemaining = Math.sqrt(dx * dx + dy * dy);
-	var dashIndex = 0;
-	while (distRemaining >= 0.1) {
-		var dashLength = Math.min(distRemaining, dashArray[dashIndex % dashCount]);
-		var step = Math.sqrt(dashLength * dashLength / (1 + slope * slope));
-		if (xSlope) {
-			if (dx < 0) step = -step;
-			x += step
-			y += slope * step;
-		} else {
-			if (dy < 0) step = -step;
-			x += slope * step;
-			y += step;
+		this.moveTo(x, y);
+		var distRemaining = Math.sqrt(dx * dx + dy * dy);
+		var dashIndex = 0;
+		while (distRemaining >= 0.1) {
+			var dashLength = Math.min(distRemaining, dashArray[dashIndex % dashCount]);
+			var step = Math.sqrt(dashLength * dashLength / (1 + slope * slope));
+			if (xSlope) {
+				if (dx < 0) step = -step;
+				x += step
+				y += slope * step;
+			} else {
+				if (dy < 0) step = -step;
+				x += slope * step;
+				y += step;
+			}
+			this[(dashIndex % 2 == 0) ? 'lineTo' : 'moveTo'](x, y);
+			distRemaining -= dashLength;
+			dashIndex++;
 		}
-		this[(dashIndex % 2 == 0) ? 'lineTo' : 'moveTo'](x, y);
-		distRemaining -= dashLength;
-		dashIndex++;
-	}
 }
 
-window.onload = function() {
+window.onload = function () {
 	var HORIZONTAL_SPACING = 20, //水平间隔
 		HORIZONTAL_SCALE = 10, //水平刻度
 		VERTICAL_SPACING = 20, //垂直间隔
@@ -63,3 +63,67 @@ window.onload = function() {
 	}
 	context.stroke();
 }
+
+var GraphLiveStore = {
+	model: '',
+	data: []
+};
+
+var GraphLive = function (opts, my) {
+	var that = {}, //用于放置私有变量
+		my = my || {}, //用于放置共享变量和函数
+		opts = opts || {
+			//初始化参数设置
+			renderTo: false, //渲染位置
+			width: 100, //宽度
+			height: 100, //高度
+			store: store, //绑定数据存储对象
+			style: {
+				borderType: 'dotted', //支持点划线(dotted)和虚线(dash)以及实线(solid)
+				borderColor: 'darkgray',
+				borderStyle: 'solid',
+				borderThickness: 1
+			},
+			axes: [{
+					type: 'Numeric', //轴数据类型：支持Numeric、Date、Time和String
+					position: 'left', //位置：支持left、right、bottom、top
+					fields: false, //轴绑定字段
+					grid: {
+						type: 'dotted', //支持点划线(dotted)和虚线(dash)以及实线(solid)
+						color: 'darkgray', //网格线颜色
+						thickness: 1 //网格线宽
+					}
+				}, {
+					type: 'Time', //轴数据类型：支持Numeric、Date、Time和String
+					position: 'bottom', //位置：支持left、right、bottom、top
+					fields: false, //轴绑定字段
+					grid: {
+						type: 'dotted', //支持点划线(dotted)和虚线(dash)以及实线(solid)
+						color: 'darkgray', //网格线颜色
+						thickness: 1 //网格线宽
+					}
+				}
+			],
+			serials: [{
+					type: 'line', //序列类型，支持两种渲染方式，一种是甘特图(gantt)类型，另一种是折线图(line)类型
+					axis: false, //绑定到的轴位置
+					xField: false, //x轴字段
+					yField: false, //y轴字段
+					label: {
+						field: false,
+						color: 'black',
+						contrast: true
+					},
+					style: {
+						color: 'green',
+						opacity: 0.93
+					},
+					highlight: {
+						size: 7,
+						radius: 7
+					}
+				}
+			]
+		};
+	return that;
+};
